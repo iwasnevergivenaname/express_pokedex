ff496031f765164311e4c41027356ea8c90d0dca
iwasnevergivenaname <hannah.mace93@gmail.com>
2020-08-07 16:06:49 -0700

    inital set up

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-const axios = require('axios'); 
+const axios = require('axios');
 const ejsLayouts = require('express-ejs-layouts');
 const app = express();
 const port = process.env.PORT || 3000;
@@ -11,10 +11,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(ejsLayouts);
 
 // GET - main index of site
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
   let pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/';
   // Use request to call the API
   axios.get(pokemonUrl).then(response => {
+    console.log(response.data);
     let pokemon = response.data.results;
     res.render('index', { pokemon: pokemon.slice(0, 151) });
   });
